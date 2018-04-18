58a6f695c95ee211752610f07b3e2bedb09e933f
drobnik <drobniklukasz@gmail.com>
2018-04-18 19:53:14 +0200

    add selection url

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,3 +2,5 @@
 // export const BASE_URL = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&format=json"; //basic URL address
 
 export const BASE_URL = "https://api.punkapi.com/v2/beers"; //basic URL address
+
+export const SELECTION = "&select=pl_hostname,pl_letter,pl_radj,pl_rade,pl_dens,pl_eqt,pl_discmethod,pl_pnum,pl_orbper,pl_orbeccen,pl_orbincl,pl_bmassj,pl_bmasse,st_dist,st_optmag"; //bit of the URL address to select the needed columns
