1d328c026ee41cd45efdc1ea2752bdbc65d2af69
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-03-24 17:45:17 +0100

    correct typo

--- src/containers/Planet Panel/Planet Tile/PlanetTile.js
+++ src/containers/Planet Panel/Planet Tile/PlanetTile.js
@@ -109,9 +109,9 @@ export class PlanetTile extends Component {
             }
         } else {
 
-            //if the planet has no definied radius (the value is 'null'), apply the following default width [flaticon font size] and set the image of an unknown planet
+            //if the planet has no defined radius (the value is 'null'), apply the following default width [flaticon font size] and set the image of an unknown planet
             imageSize = "40px";
-            imageName = "unknown.png";
+            imageName = "unknown.png";gi
 
         }
 
