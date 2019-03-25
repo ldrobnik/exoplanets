0741176801a2f0a5b1cf6373422ad8a4631e392b
Łukasz Drobnik <drobniklukasz@gmail.com>
2019-03-25 10:22:03 +0100

    add lazy loading of DetailedView component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, {Component} from "react";
+import React, {Component, Suspense} from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
 import Home from "./containers/Home/Home";
-import DetailedView from "./containers/Detailed View/DetailedView";
+// import DetailedView from "./containers/Detailed View/DetailedView";
 
+
+const DetailedView = React.lazy(() => {
+    return import("./containers/Detailed View/DetailedView");
+});
 
 class App extends Component {
 
@@ -13,7 +17,7 @@ class App extends Component {
             <Router>
                 <div>
                     <Route path="/" component={Home}/>
-                    <Route path="/details/:hostName/:planetLetter" component={DetailedView}/>
+                    <Suspense fallback="..."><Route path="/details/:hostName/:planetLetter" component={DetailedView}/></Suspense>
                 </div>
             </Router>
         );
